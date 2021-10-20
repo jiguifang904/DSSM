101257892be48bfa713fa7eaa76cfe8a86147894
jiguifang904 <49645291+jiguifang904@users.noreply.github.com>
2021-10-20 15:19:24 +0800

    Add files via upload

---
+++ layers/core.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 17:42:06 2021
+
+@author: jiguifang
+"""
+
+import torch
+import torch.nn as nn
+from layers.activation import activation_layer  # 激活函数选择
+
+
+class PredictionLayer(nn.Module):   # 预测类
+    def __init__(self, task='binary', use_bias=True, **kwargs):
+        if task not in ["binary", "multiclass", "regression"]:
+            raise ValueError("task must be binary,multiclass or regression")
+
+        super(PredictionLayer, self).__init__()
+        self.use_bias = use_bias
+        self.task = task
+        if self.use_bias:
+            self.bias = nn.Parameter(torch.zeros((1,)))
+    
+    
+    def forward(self, X):
+        output = X
+        if self.use_bias:
+            output += self.bias
+        if self.task == "binary":
+            output = torch.sigmoid(X)
+        return output
+    
+
+class DNN(nn.Module):  # 多层全连接（激活函数，batch_normld)
+    def __init__(self, inputs_dim, hidden_units, activation='relu', l2_reg=0, dropout_rate=0, use_bn=False,
+                 init_std=0.0001, dice_dim=3, seed=1024, device='cpu'):
+        super(DNN, self).__init__()
+        self.dropout_rate = dropout_rate
+        self.dropout = nn.Dropout(dropout_rate)
+        self.seed = seed
+        self.l2_reg = l2_reg
+        self.use_bn = use_bn  # 是否用batch_normld层
+        if len(hidden_units) == 0:
+            raise ValueError("hidden_units is empty!!")
+            
+        hidden_units = [inputs_dim] + list(hidden_units)  # 多层全连接
+
+        self.linears = nn.ModuleList(
+            [nn.Linear(hidden_units[i], hidden_units[i+1]) for i in range(len(hidden_units) - 1)])
+        # 上一层的输出是下一层的输入
+        
+        if self.use_bn:
+            self.bn = nn.ModuleList(
+                [nn.BatchNorm1d(hidden_units[i+1]) for i in range(len(hidden_units) - 1)])
+            
+        self.activation_layers = nn.ModuleList(
+            [activation_layer(activation, hidden_units[i+1], dice_dim) for i in range(len(hidden_units) - 1)])
+
+        for name, tensor in self.linears.named_parameters():
+            if 'weight' in name:   #初始化
+                nn.init.normal_(tensor, mean=0, std=init_std)
+
+        self.to(device)
+        
+    
+    def forward(self, inputs):
+        deep_input = inputs
+        for i in range(len(self.linears)):
+            fc = self.linears[i](deep_input)
+        
+            if self.use_bn:
+                fc = self.bn[i](fc)
+    
+            fc = self.activation_layers[i](fc)
+    
+            fc = self.dropout(fc)
+            deep_input = fc
+            
+        return deep_input
+            
+
+class LocalActivationUnit(nn.Module):
+    def __init__(self, hidden_units=(64, 32), embedding_dim=4, activation='sigmoid', dropout_rate=0,
+                 dice_dim=3, l2_reg=0, use_bn=False):
+        super(LocalActivationUnit, self).__init__()
+
+        self.dnn = DNN(inputs_dim=4 * embedding_dim,
+                       hidden_units=hidden_units,
+                       activation=activation,
+                       l2_reg=l2_reg,
+                       dropout_rate=dropout_rate,
+                       dice_dim=dice_dim,
+                       use_bn=use_bn)
+
+        self.dense = nn.Linear(hidden_units[-1], 1)    # 最后一层全连接
+        
+    
+    def forward(self, query, user_behavier):
+        # query ad            : size -> batch_size * 1 * embedding_size
+        # user behavior       : size -> batch_size * time_seq_len * embedding_size
+        
+        user_behavier_len = user_behavier.size(1)
+
+        queries = query.expand(-1, user_behavier_len, -1)
+
+        attention_input = torch.cat([queries, user_behavier, queries - user_behavier, queries * user_behavier],
+                                    dim=-1)    # [B, T, 4*E]
+        attention_out = self.dnn(attention_input)
+
+        attention_score = self.dense(attention_out)    # [B, T, 1]
+
+        return attention_score
