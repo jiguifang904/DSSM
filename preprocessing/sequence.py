b1e89627e73a440420dc8bf281f37c92ba798383
jiguifang904 <49645291+jiguifang904@users.noreply.github.com>
2021-10-20 15:20:10 +0800

    Add files via upload

---
+++ preprocessing/sequence.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 18:13:30 2021
+
+@author: jiguifang
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from layers.core import LocalActivationUnit
+
+
+class SequencePoolingLayer(nn.Module):    # 稀疏边长特征进行pooling操作
+    def __init__(self, mode='mean', support_masking=False, device='cpu'):
+        super(SequencePoolingLayer, self).__init__()
+        if mode not in ['sum', 'mean', 'max']:
+            raise ValueError('parameter mode should in [sum, mean, max]')
+        self.supports_masking = support_masking
+        self.mode = mode
+        self.device = device
+        self.eps = torch.FloatTensor([1e-8]).to(device)
+        self.to(device)
+        
+        
+    def _sequence_mask(self, lengths, maxlen=None, dtype=torch.bool):
+        if maxlen is None:
+            maxlen = lengths.max()
+            
+        row_vector = torch.arange(0, maxlen, 1).to(lengths.device)
+        matrix = torch.unsqueeze(lengths, dim=-1)
+        mask = row_vector < matrix   # [False,  True,...]满足条件为True，否则为False
+        
+        mask.type(dtype)
+        return mask
+    
+    
+    def forward(self, seq_value_len_list):
+        if self.supports_masking:
+            uiseq_embed_list, mask = seq_value_len_list  # [B, T, E], [B, 1]
+            mask = mask.float()
+            user_behavior_length = torch.sum(mask, 1, keepdim=True)
+            mask = mask.unsqueeze(2)
+        else:
+            uiseq_embed_list, user_behavior_length = seq_value_len_list    # [B, T, E], [B, 1]
+            mask = self._sequence_mask(user_behavior_length, maxlen=uiseq_embed_list.shape[1], dtype=torch.float32)
+            mask = torch.transpose(mask, 1, 2)
+            
+        embedding_size = uiseq_embed_list.shape[-1]
+        
+        mask = torch.repeat_interleave(mask, embedding_size, dim=2)  # [B, maxlen, E]
+        # 复制tensor， torch.repeat_interleave(tensor，复制次数， 维度)
+        
+        if self.mode == "max":
+            hist = uiseq_embed_list - (1 - mask) * 1e9
+            hist = torch.max(hist, dim=1, keepdim=True)[0]
+            return hist
+        
+        hist = uiseq_embed_list * mask.float()
+        hist = torch.sum(hist, dim=1, keepdim=False)
+        
+        if self.mode == 'mean':
+            self.eps = self.eps.to(user_behavior_length.device)
+            hist = torch.div(hist, user_behavior_length.type(torch.float32) + self.eps)
+        
+        hist = torch.unsqueeze(hist, dim=1)
+        return hist
+
+        
+class AttentionSequencePoolingLayer(nn.Module):
+    def __init__(self, att_hidden_units=(80, 40), att_activation='sigmoid', weight_normalization=False,
+                 return_score=False, supports_masking=False, embedding_dim=4, **kwargs):
+        super(AttentionSequencePoolingLayer, self).__init__()
+        self.return_score = return_score
+        self.weight_normalization = weight_normalization
+        self.supports_masking = supports_masking
+        self.local_att = LocalActivationUnit(hidden_units=att_hidden_units, embedding_dim=embedding_dim,
+                                             activation=att_activation, dropout_rate=0, 
+                                             use_bn=False)   # use_bn是否使用batchnorm
+        
+    
+    def forward(self, query, keys, keys_length, mask=None):
+        batch_size, max_length, _ = keys.size()
+        
+        
+        if self.supports_masking:
+            if mask in None:
+                raise ValueError("When supports_masking=True,input must support masking")
+            keys_masks = mask.unsqueeze(1)
+        else:
+            keys_masks = torch.arange(max_length, device=keys_length.device,
+                                      dtype=keys_length.dtype).repeat(batch_size, 1)  # [B, T]
+            keys_masks = keys_masks < keys_length.view(-1, 1)  # 0, 1 mask
+            keys_masks = keys_masks.unsqueeze(1)  # [B, 1, T]
+            
+        attention_score = self.local_att(query, keys)  # [B, T, 1]
+
+        outputs = torch.transpose(attention_score, 1, 2)  # [B, 1, T]
+        
+        
+        if self.weight_normalization:
+            paddings = torch.ones_like(outputs) * (-2 ** 32 + 1)
+        else:
+            paddings = torch.zeros_like(outputs)
+            
+        
+        outputs = torch.where(keys_masks, outputs, paddings)  # [B, 1, T]
+        # torch.where()函数的作用是按照一定的规则合并两个tensor类型, 合并outputs, padding
+        
+        if self.weight_normalization:
+            outputs = F.softmax(outputs, dim=-1)  # [B, 1, T]
+
+        if not self.return_score:
+            # Weighted sum
+            outputs = torch.matmul(outputs, keys)  # [B, 1, E]
+
+        return outputs
+
+        
+
+if __name__ == "__main__":
+    row_vector = torch.arange(0, 10, 1)
+    matrix =  torch.arange(0, 20, 2)
+    
+    print(row_vector)
+    mask = row_vector < matrix
+    print(mask)
+    print(1, mask.type(torch.bool))
+    row_vector = row_vector.unsqueeze(1)
+    print(row_vector.size())
+    padding = torch.ones_like(row_vector)
+    print(padding)
+    print(torch.where(mask, row_vector, padding))
